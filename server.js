d5ea37a5d46c181afb4565f11605c049df7fb477
PABLO MORAGA <otello1971@yahoo.com>
2022-11-08 20:46:29 -0300

    Prueba 01 --- Veamos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<b>Hello World</b></br>ECest -Bonjour monde- en francais.');
+  res.send('<b>Hello World</b></br>C&#39;est -Bonjour monde- en fran&#231;ais.');
 });
 
 app.listen(PORT, HOST);
